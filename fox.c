0acb97d077616f835951f5c222c37e2320317230
Lucas Rocha <luckasrochaccb@gmail.com>
2017-11-27 18:58:54 -0300

    README

--- fox.c
+++ fox.c
@@ -4,7 +4,7 @@
 const int MAX_TAM = 8;
 
 /*
-* Aluno1: Gabriel Escobar Paies | 2014.1907.
+* Aluno1: Gabriel Escobar Paes  | 2014.1907.021-5
 * Aluno2: Lucas Barbosa Rocha   | 2014.1907.013-4
 * Algoritmos Paralelos - Henrique Mongelli
 * Algoritmo: Multiplicação de matriz - FOX
